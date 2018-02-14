f9421e644a78fc5b58b7381ea2659a4ff7e94b78
_kbluue_ <kbluue@yahoo.com>
2018-02-14 12:28:14 +0100

    ALR add

--- src/Cracking_The_Code_INTERVIEWS/Arrays__Left_Rotation.java
+++ src/Cracking_The_Code_INTERVIEWS/Arrays__Left_Rotation.java
@@ -1,5 +1,8 @@
 package Cracking_The_Code_INTERVIEWS;
 
+import java.util.Arrays;
+import java.util.Scanner;
+
 /**
  * Created by _kbluue_ on 1/29/2018.
  * 
@@ -7,7 +10,32 @@ package Cracking_The_Code_INTERVIEWS;
  */
 public class Arrays__Left_Rotation {
 
-    static void run(int noOfIntegers,int noOfLeftRotation,int... arrayContent){
-        if (arrayContent.le)
+    private static int[] swap(int noOfIntegers,int noOfLeftRotation,int... arrayContent){
+        if (arrayContent.length != noOfIntegers) System.out.println("Input Error");
+        else {
+            int[] out = new int[noOfIntegers];
+            for (int i = 0; i < noOfIntegers; i++){
+                int index = i + noOfLeftRotation;
+                if (index >= noOfIntegers) index =- noOfIntegers;
+                out[index] = arrayContent[i];
+            }
+            return out;
+        }
+        return null;
+    }
+
+    static void run(){
+        //read
+        Scanner in = new Scanner(System.in);
+        int noOfIntegers = in.nextInt();
+        int noOfLeftRotation = in.nextInt();
+        int arrayContent[] = new int[noOfIntegers];
+        for (int i = 0; i < noOfIntegers; i++) arrayContent[i] = in.nextInt();
+
+        //swap
+        int swappedArray[] = swap(noOfIntegers, noOfLeftRotation, arrayContent);
+
+        //print
+        System.out.println(Arrays.toString(swappedArray));
     }
 }
