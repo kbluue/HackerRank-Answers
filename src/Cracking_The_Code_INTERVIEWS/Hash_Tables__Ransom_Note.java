2bbfee2f53a2d45b1e155fed872b4be899b5fbc9
_kbluue_ <kbluue@yahoo.com>
2018-02-15 11:06:49 +0100

    HTRN add

---
+++ src/Cracking_The_Code_INTERVIEWS/Hash_Tables__Ransom_Note.java
@@ -0,0 +1,36 @@
+package Cracking_The_Code_INTERVIEWS;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+/**
+ * Created by _kbluue_ on 2/15/2018.
+ *
+ * Check https://www.hackerrank.com/challenges/ctci-ransom-note/problem for full question
+ */
+public class Hash_Tables__Ransom_Note {
+
+    private boolean mapMatch(String magazine[],String ransom[]){
+        Map<String, Integer> wordBank = new HashMap<>();
+        for (String word : magazine) {
+            if (wordBank.get(word) == null) wordBank.put(word, 1);
+            else wordBank.put(word, wordBank.get(word) + 1);
+        }
+        for (String word : ransom) {
+            if (wordBank.get(word) == null || wordBank.get(word) == 0) return false;
+            wordBank.put(word, wordBank.get(word) - 1);
+        }
+        return true;
+    }
+
+    static void run(){
+        //read
+        Scanner in = new Scanner(System.in);
+        in.nextLine(); //redundant since I'm using String.split
+        String magazine = in.nextLine();
+        String ransom = in.nextLine();
+        //run and print
+        System.out.println(new Hash_Tables__Ransom_Note().mapMatch(magazine.split(" "), ransom.split(" ")));
+    }
+}
